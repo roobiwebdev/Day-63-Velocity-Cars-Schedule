dea03b70da93345a7ffa1eb30f22241750090623
Rhobot Mulatu <mulaturhobot7@gmail.com>
2024-12-16 21:05:29 +0300

    main.js

--- src/main.js
+++ src/main.js
@@ -25,58 +25,51 @@ navLinks.forEach((link) => {
   });
 });
 
-if (window.innerWidth >= 760) {
-  const sr = ScrollReveal({
+if(window.innerWidth >= 760){
+  ScrollReveal({
     origin: "top",
     distance: "60px",
-    duration: 2500,
-    delay: 300,
+    duration: "2500",
+    delay: "200",
     reset: true,
   });
-
-  sr.reveal(".home__data");
-  sr.reveal(".head__data", { delay: 200 });
-  sr.reveal(".home__img", { delay: 500, scale: 0.5 });
-
-  sr.reveal(".service__card", { distance: "100px" });
-  sr.reveal(".service__card_1", { delay: 100 });
-  sr.reveal(".service__card_2", { delay: 300 });
-  sr.reveal(".service__card_3", { delay: 500 });
-  sr.reveal(".service__card_4", { delay: 700 });
-
-  sr.reveal(".about__cont");
-  sr.reveal(".about__leaf", { origin: "right" });
-  sr.reveal(".about__item_img", { distance: "400px", origin: "left" });
-  sr.reveal(".about__item_disc", { distance: "400px", origin: "right" });
-
-  sr.reveal(".about__item2_img", { distance: "400px", origin: "right" });
-  sr.reveal(".about__item2_disc", { distance: "400px", origin: "left" });
-
-  sr.reveal(".popular__card", { distance: "100px" });
-  sr.reveal(".popular__card_1", {
+  
+  ScrollReveal().reveal(".nav__data, .footer__data");
+  ScrollReveal().reveal(".home__data");
+  ScrollReveal().reveal(".home__title", { scale: "0.5" });
+  ScrollReveal().reveal(".white__car", { delay: "1100", distance: "800px", origin: "left" });
+  
+  ScrollReveal().reveal(".home__right", { distance: "400px", origin: "bottom" });
+  ScrollReveal().reveal(".home__left", { distance: "-400px", origin: "bottom" });
+  
+  ScrollReveal().reveal(".explor__data");
+  ScrollReveal().reveal(".red__car", { delay: "1100", distance: "-800px", origin: "left" });
+  
+  ScrollReveal().reveal(".home__right", { distance: "400px", origin: "bottom" });
+  
+  ScrollReveal().reveal(".cards", { distance: "100px" });
+  ScrollReveal().reveal(".card1, .why__card1", {
     delay: 100,
     origin: "left",
     distance: "400px",
   });
-  sr.reveal(".popular__card_2", {
+  ScrollReveal().reveal(".card2, .why__card2", {
     delay: 300,
     origin: "bottom",
     distance: "300px",
   });
-  sr.reveal(".popular__card_3", {
+  ScrollReveal().reveal(".card3, .why__card3", {
     delay: 500,
     origin: "top",
     distance: "300px",
   });
-  sr.reveal(".popular__card_4", {
-    delay: 700,
-    origin: "right",
-    distance: "400px",
-  });
-
-  sr.reveal(".customer__review");
-
-  sr.reveal(".footer__cont");
-
+  ScrollReveal().reveal(".why__card4", { delay: 500, origin: "top", distance: "300px" });
+  
+  ScrollReveal().reveal(".try__data");
+  
+  ScrollReveal().reveal(".curve_car,.why__Carimg", { scale: "0.3", delay: "400" });
+  
+  ScrollReveal().reveal(".why__data");
+  
 }
 
